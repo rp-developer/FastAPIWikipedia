f3ab8ab330fc791d0f4bd96136011b2d058b3aca
Ryan Pfeiffer <ryanpfeiffer@Ryans-MacBook-Air.local>
2025-01-07 21:32:19 -0500

    improved error handling

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ async def home(request: Request):
 
 @app.get("/submit", response_class=HTMLResponse)
 async def page(request: Request, query: Optional[str] = Query(None)):
-    if query is None:
+    if query == None:
         error = "No query parameters detected"
         return templates.TemplateResponse("404.html", {
             "error": error,
@@ -34,7 +34,7 @@ async def page(request: Request, query: Optional[str] = Query(None)):
         }, status_code=400)
     
     query = query.replace(" ", "_").title()
-
+    
     encodedQuery = quote_plus(query)
     redis_data = await redis_client.get(encodedQuery)
     print(redis_data)
@@ -50,6 +50,13 @@ async def page(request: Request, query: Optional[str] = Query(None)):
     else:
         async with httpx.AsyncClient() as client:
             response = await client.get(f"https://en.wikipedia.org/api/rest_v1/page/summary/{(query)}", follow_redirects=True)
+            print(response.status_code)
+            if response.status_code == 404:
+                error = "Page not found"
+                return templates.TemplateResponse("404.html", {
+                    "error": error,
+                    "request": request
+                })
             responseData = response.json()
             summary = responseData['extract']
             page = responseData['content_urls']['desktop']['page']
@@ -81,16 +88,24 @@ async def api(request: Request, query: Optional[str] = Query(None)):
         return {
             "page": cached_data['page'], 
             "summary": cached_data['summary'], 
-            "title": cached_data['title']
+            "title": cached_data['title'],
             }
     
     else:
         async with httpx.AsyncClient() as client:
             response = await client.get(f"https://en.wikipedia.org/api/rest_v1/page/summary/{(query)}", follow_redirects=True)
+            print(response.status_code)
+            if response.status_code == 404:
+                error = "Page not found"
+                return {
+                    "error": error,
+                }
             responseData = response.json()
-            summary = responseData['extract']
-            page = responseData['content_urls']['desktop']['page']
-            title = responseData['title']
+            if 'extract' in responseData and 'content_urls' in responseData:
+                summary = responseData['extract']
+                page = responseData['content_urls']['desktop']['page']
+                title = responseData['title']
+            
 
             redisSetData = {
                 'page': page, 
